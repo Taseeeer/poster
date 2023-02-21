4a9c2587a2527c1b79e04cac2cf81e425826691f
Syed Taseer Shah <66362477+Taseeeer@users.noreply.github.com>
2023-02-21 10:46:15 +0500

    schema settled

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,41 @@
+'use client';
+
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import './globals.css'
+
+const theme = extendTheme({
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+  styles: {
+    global: (props) => ({
+      'html, body': {
+        fontSize: 'sm',
+        color: props.colorMode === 'dark' ? 'whitesmoke' : 'gray.600',
+        lineHeight: 'tall',
+      },
+      a: {
+        color: props.colorMode === 'dark' ? 'teal.300' : 'teal.500',
+      },
+    }),
+  },
+  colors: {
+    gray: {
+      100: 'rgba(0,0,0,0.1)',
+      200: 'rgba(0,0,0,0.8)',
+    },
+    white: {
+      100: 'whitesmoke',
+    },
+  }
+});
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  
   return (
     <html lang="en">
       {/*
@@ -12,7 +43,13 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <ChakraProvider theme={theme}>
+          {/* <Box bgGradient="linear(to-r, blue.300, blue.400, blue.500)"> */}
+            {children}
+          {/* </Box> */}
+        </ChakraProvider>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,45 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+'use client';
 
-const inter = Inter({ subsets: ['latin'] })
+import { Card, CardBody, Text, Heading, Button, Center, Box, Flex, useColorMode } from '@chakra-ui/react'
+import { Inter } from '@next/font/google';
+import Post from './post/page';
+import { SiNextdotjs, SiChakraui, SiRailway, SiPrisma } from "react-icons/si";
+import { HiSun, HiMoon } from 'react-icons/hi'
 
 export default function Home() {
+
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Box>
+      <Flex justify="flex-end" padding="1rem" cursor="pointer">
+        { colorMode === 'light' ? 
+          <HiSun onClick={toggleColorMode} style={{ fontSize: "1.5rem"}} />
+          :
+          <HiMoon onClick={toggleColorMode} style={{ fontSize: "1.5rem"}} />
+        }
+      </Flex>
+      <Card width={{ base: "350px", lg:"500px"}} textAlign="center" marginX="auto" paddingY="2rem" marginY="1rem" bgGradient='linear(to-r, green.200, pink.500)'>
+        <CardBody>
+          <Heading>Welcome to Poster.</Heading>
+          <Text width="25ch" marginX="auto">Post enables you to post your stuff in here.</Text>
+          <Button bgColor="gray.100" _hover={{ bgColor: ''}} marginTop="1rem">Sign In</Button>
+        </CardBody>
+      </Card>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Box>
+        <Center>
+          <Text>This application is powered by</Text>
+        </Center>
+        <Center paddingY="2rem">
+          <Flex fontSize="3rem" gap="2rem">
+            <SiNextdotjs />
+            <SiChakraui />
+            <SiRailway />
+            <SiPrisma />
+          </Flex>
+        </Center>
+      </Box>
+    </Box>
   )
 }
